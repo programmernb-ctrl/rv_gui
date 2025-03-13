95b40f579e18089ffb0aad2e08765dde4f40b5d4
programmernb-ctrl <141086417+programmernb-ctrl@users.noreply.github.com>
2025-03-13 20:16:52 +0100

    hotfix, changed base path in vite config, and changed router to use web hash history

--- gui/src/router/router.ts
+++ gui/src/router/router.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import {createRouter, createWebHashHistory } from 'vue-router';
 import MainView from '../views/MainView.vue';
 
 const routes = [
@@ -10,13 +10,14 @@ const routes = [
     {
         path: '/test',
         name: 'test',
-        component: () => import ('../views/TestView.vue'), // Dynamic Import
+        component: () => import('../views/TestView.vue'), // Dynamic Import
     },
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes,
 });
 
 export default router;
+
