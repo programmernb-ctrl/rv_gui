f9a1f396622b3abfdc5c3e1946ff60355a15c18e
programmernb-ctrl <141086417+programmernb-ctrl@users.noreply.github.com>
2025-06-10 12:40:28 +0200

    refactored client code

--- src/client/cb.ts
+++ src/client/cb.ts
@@ -1,86 +1,126 @@
-import { waitFor, triggerServerCallback, sleep } from '@communityox/ox_lib/client';
-import { useWebview } from './useWebview';
+import {waitFor, triggerServerCallback, sleep, cache} from '@communityox/ox_lib/client';
+import {useWebview} from './useWebview';
+import {debugLog} from '@/common/Config';
 
 const Webview = useWebview();
 
 onNet('ox:startCharacterSelect', async (userId: number, ...characters) => {
-   if (!userId || !characters) return;
+    if (!userId || !characters) return;
 
-   const sessionStarted = await waitFor(() => {
-      return NetworkIsSessionStarted();
-   }, 'Failed to wait for session start..', 10000);
+    const sessionStarted = await waitFor(
+        () => {
+            return NetworkIsSessionStarted();
+        },
+        'Failed to wait for session start..',
+        10000,
+    );
 
-   if (!sessionStarted) {
-      return
-   }
+    if (!sessionStarted) {
+        return;
+    }
 
-   setTimeout(async () => {
-      const args = [userId, characters]
-      const response = await triggerServerCallback<{ serverValue: number }>('test:server', 1, args)
-      if (!response) return;
+    setTimeout(async () => {
+        const args = [userId, characters];
+        const response = await triggerServerCallback<{serverValue: number}>('test:server', 1, args);
+        if (!response) return;
 
-      console.log(`Response from server: ${JSON.stringify(response)}`);
+        debugLog(`Response from server: ${JSON.stringify(response)}`);
 
-      LocalPlayer.state.set('webview', 'opened', false)
+        SendNUIMessage({
+            type: 'isVisible',
+            value: true,
+        });
 
-      SendNUIMessage({
-         type: 'isVisible',
-         value: true
-      })
+        await sleep(500);
 
-      await sleep(10)
+        if (Webview.getPage() !== 'hud') {
+            Webview.setPage('hud');
+        }
 
-      if (Webview.getPage() !== 'hud') {
-         Webview.setPage('hud');
-      }
-   }, 50);
-})
+        LocalPlayer.state.set('webview', 'opened', false);
+    }, 50);
+});
 
 // Example to get player data in the webview
 RegisterNuiCallbackType('playerData');
 on('__cfx_nui:playerData', async (data, cb) => {
-   const playerData = data;
-   if (!playerData) return;
+    let netID: number = 0;
+    let tried: number = 0;
 
-   let netID: number = 0;
-   let tried: number = 0;
+    while (netID === 0 && tried < 10) {
+        netID = NetworkGetNetworkIdFromEntity(PlayerPedId());
+        tried++;
+        await sleep(1000);
+    }
 
-   while (netID === 0 && tried < 10) {
-      netID = NetworkGetNetworkIdFromEntity(PlayerPedId());
-      tried++;
-      await sleep(1000);
-   }
+    if (netID > 0) {
+        const playerName = GetPlayerName(PlayerId());
+        const playerCoords = GetEntityCoords(PlayerPedId());
 
-   if (netID > 0) {
-      const playerName = GetPlayerName(PlayerId());
-      const playerCoords = GetEntityCoords(PlayerPedId());
-
-      cb({
-         id: netID,
-         name: playerName,
-         coords: playerCoords
-      });
-   } else {
-      cb({
-         id: 0,
-         name: 'Unknown',
-         coords: { x: 0, y: 0, z: 0 }
-      });
-   }
-})
-
-AddStateBagChangeHandler('webview', '', async (bagName: string, key: string, value: any) => {
-   if (bagName && key && value) {
-      const playerFromBag = GetEntityFromStateBagName(bagName)
-
-      const entityExists = await waitFor(() => {
-         return DoesEntityExist(playerFromBag)
-      }, 'Entity does not exist!', 1000)
-
-      if (entityExists === true) {
-         Citizen.trace(`Got Player ${playerFromBag} from StateBag...`);
-
-         // emit('rv_gui:client:UpdateData', value as boolean)
-      }
-   }
+        cb({
+            type: 'playerData',
+            value: {
+                id: netID,
+                name: playerName,
+                coords: playerCoords,
+            },
+        });
+    } else {
+        cb({
+            type: 'playerData',
+            value: {
+                id: 0,
+                name: 'Unknown',
+                coords: {x: 0, y: 0, z: 0},
+            },
+        });
+    }
 });
+
+AddStateBagChangeHandler(
+    'webview',
+    `player:${cache.serverId}`,
+    async (bagName: string, key: string, value: string, reserved: number, replicated: boolean) => {
+        if (bagName && key && value && replicated !== true) {
+            const playerFromBag = GetEntityFromStateBagName(bagName);
+
+            const entityExists = await waitFor(
+                () => {
+                    return NetworkIsSessionStarted();
+                },
+                'Session is not started!',
+                10000,
+            );
+
+            if (!entityExists) {
+                console.log(`^3[rv_gui] Boilerplate^0 :: Session is not started... !`);
+                return;
+            }
+
+            if (value === 'opened') {
+                if (Webview.getPage() !== 'hud') {
+                    return;
+                }
+
+                setTick(async () => {
+                    if (
+                        cache.vehicle ||
+                        IsPedWalking(cache.ped) ||
+                        IsPedRunning(cache.ped) ||
+                        IsPedSprinting(cache.ped)
+                    ) {
+                        SendNUIMessage({
+                            type: 'playerData',
+                            value: {
+                                id: cache.serverId,
+                                name: GetPlayerName(playerFromBag),
+                                coords: GetEntityCoords(cache.ped),
+                            },
+                        });
+                    }
+                    await sleep(100);
+                });
+            }
+        }
+    },
+);
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,98 +1,96 @@
 import './cb';
-import { cache, sleep } from '@communityox/ox_lib';
-import { Ped, Vehicle, VehicleSeat } from '@nativewrappers/fivem';
-import { useWebview, validPages } from './useWebview';
+import {sleep, cache} from '@communityox/ox_lib/client';
+import {Ped, Vehicle, VehicleSeat} from '@nativewrappers/fivem';
+import {useWebview, validPages} from '@/client/useWebview';
+import {debugLog} from '@/common/Config';
 
 const webview = useWebview();
 
 onNet('rv_gui:Webview:switch', async (arg: keyof typeof validPages, focus?: number) => {
-   await sleep(1);
+    await sleep(1);
 
-   Citizen.trace(`^3[rv_gui] Boilerplate^0 | arguments at webview switch event received ^3{ ${arg}, ${focus} }^0`);
+    debugLog(`^3[rv_gui] Boilerplate^0 | arguments at webview switch event received ^3{ ${arg}, ${focus} }^0`);
 
-   try {
-      webview.setPage(arg);
+    try {
+        webview.setPage(arg);
 
-      if (webview.getPage() === arg && focus === 1) {
-         webview.focus();
-      } else if (webview.getPage() === arg && focus === 0) {
-         webview.unfocus();
-      } else return;
-   } catch (error) {
-      Citizen.trace(`^3[rv_gui] Boilerplate^0 | error for webview switch event occured ^1'${error}'^0`)
-   }
-})
+        if (webview.getPage() === arg && focus === 1) {
+            webview.focus();
+        } else if (webview.getPage() === arg && focus === 0) {
+            webview.unfocus();
+        } else return;
+    } catch (error) {
+        debugLog(`^3[rv_gui] Boilerplate^0 | error for webview switch event occured ^1'${error}'^0`);
+    }
+});
 
 on('gameEventTriggered', (name: string, args) => {
-   if (name === 'CEventNetworkPlayerEnteredVehicle')
-      emit('rv_gui:client:enteredVeh', args[0], args[1])
+    if (name === 'CEventNetworkPlayerEnteredVehicle') emit('rv_gui:client:enteredVeh', args[0], args[1]);
 });
 
 on('rv_gui:client:enteredVeh', (netId: number, vehId: number) => {
-   console.log(`Yeah called enteredVeh with netID ${netId} and vehicleID ${vehId}`);
+    debugLog(`called enteredVeh with netID ${netId} and vehicleID ${vehId}`);
 
-   const pedVehicleIsIn = GetVehiclePedIsIn(cache.ped, false);
-   const getPedFromSeat = GetPedInVehicleSeat(vehId, -1);
+    const pedVehicleIsIn = GetVehiclePedIsIn(cache.ped, false);
+    const getPedFromSeat = GetPedInVehicleSeat(vehId, -1);
 
-   if (pedVehicleIsIn !== 0 && getPedFromSeat === cache.ped) {
-      LocalPlayer.state.set('inVehicle', true, false);
-   }
-})
+    if (pedVehicleIsIn !== 0 && getPedFromSeat === cache.ped) {
+        LocalPlayer.state.set('inVehicle', true, false);
+    }
+});
 
-AddStateBagChangeHandler('inVehicle', '', async (bagName, key, value, _, replicated) => {
-   console.log(`Bag: ${bagName}, key: ${key}, value: ${value}, isReplicated: ${replicated}`)
+AddStateBagChangeHandler(
+    'inVehicle',
+    `player:${cache.serverId}`,
+    async (bagName: string, key: string, value: boolean, reserved: number, replicated: boolean) => {
+        debugLog(`Bag: ${bagName}, key: ${key}, value: ${value}, isReplicated: ${replicated}`);
+        if (replicated === true) return;
 
-   if (!cache.vehicle) {
-      console.log('No vehicle found, returning!')
-      return
-   }
+        let statePlayer = GetPlayerFromStateBagName(bagName);
+        let playerPed = Ped.fromHandle(cache.ped);
+        let playerVehicle = Vehicle.fromHandle(GetVehiclePedIsIn(cache.ped, false));
 
-   let statePlayer = GetPlayerFromStateBagName(bagName)
-   let playerPed = Ped.fromHandle(cache.ped)
-   let playerVehicle = Vehicle.fromHandle(GetVehiclePedIsIn(cache.ped, false))
-   if (!statePlayer && !playerPed && !playerVehicle) {
-      await sleep(1)
-   }
+        while (!statePlayer && !playerPed && !playerVehicle) {
+            await sleep(100);
+            statePlayer = GetPlayerFromStateBagName(bagName);
+            playerPed = Ped.fromHandle(cache.ped);
+            playerVehicle = Vehicle.fromHandle(GetVehiclePedIsIn(cache.ped, false));
 
-   if (statePlayer > 0 && key == 'inVehicle' && value === true) {
-      SendNUIMessage({
-         type: 'isInVehicle',
-         value: true
-      })
+            if (statePlayer && playerPed && playerVehicle) {
+                break;
+            }
+        }
 
-      if (playerPed !== null && playerVehicle !== null && cache.seat !== false) {
-         runOnTick(playerPed, playerVehicle)
-      }
-   } else {
-      SendNUIMessage({
-         type: 'isInVehicle',
-         value: false
-      })
-   }
-})
+        if (typeof statePlayer === 'number' && statePlayer > 0 && key === 'inVehicle' && value === true) {
+            if (playerPed?.exists() && playerVehicle?.exists()) {
+                runOnTick(playerPed, playerVehicle);
+            }
+        }
+    },
+);
 
 function runOnTick(ped: Ped, vehicle: Vehicle) {
-   if (!ped?.exists() && !vehicle?.exists()) return;
+    if (!ped?.exists() || !vehicle?.exists()) return;
 
-   console.log(`Ped netID: ${ped.NetworkId}, vehicle netID: ${vehicle.NetworkId}`)
+    debugLog(`Ped netID: ${ped.NetworkId}, Vehicle netID: ${vehicle.NetworkId}`);
 
-   let tick = setTick(async () => {
-      await sleep(150)
+    let tick: number = setTick(async () => {
+        await sleep(150);
 
-      let pedOnSeat = vehicle.getPedOnSeat(VehicleSeat.Driver);
-      if (pedOnSeat !== null && pedOnSeat?.isInVehicle(vehicle)) {
-         if (GetEntitySpeed(vehicle.Handle) > 0) {
-            SendNUIMessage({
-               type: 'speed',
-               value: GetEntitySpeed(vehicle.Handle)
-            })
-         } else {
-            await sleep(1000);
-         }
-      } else {
-         LocalPlayer.state.set('inVehicle', false, false)
-         clearTick(tick);
-         return;
-      }
-   })
+        const seatPed = vehicle.getPedOnSeat(VehicleSeat.Driver);
+
+        if (seatPed?.isInVehicle ?? cache.vehicle) {
+            if (GetEntitySpeed(vehicle.Handle) > 0) {
+                SendNUIMessage({
+                    type: 'speed',
+                    value: GetEntitySpeed(vehicle.Handle),
+                });
+            } else {
+                await sleep(1000);
+            }
+        } else {
+            LocalPlayer.state.set('inVehicle', false, false);
+            clearTick(tick);
+        }
+    });
 }
