88dd1a5e2c3d00196d02a119b261938476b2be6e
programmernb-ctrl <141086417+programmernb-ctrl@users.noreply.github.com>
2025-06-10 10:34:14 +0200

    added useNui composable for the webview

---
+++ gui/src/composables/useNui.ts
@@ -0,0 +1,21 @@
+import {onMounted, onUnmounted} from 'vue';
+
+interface NuiMessage<T = unknown> {
+    type: string;
+    value: T;
+}
+
+export const useNui = <T = unknown>(type: string, handler: (value: T) => void) => {
+    const eventListener = (event: MessageEvent<NuiMessage<T>>) => {
+        const {type: eventType, value} = event.data;
+
+        if (eventType === type) {
+            handler(value);
+        }
+    };
+
+    onMounted(() => window.addEventListener('message', eventListener));
+    onUnmounted(() => window.removeEventListener('message', eventListener));
+
+    return {};
+};
